43220996a05ff07dd33ec421a944dcf5ba60438a
Harshal-creator <harshalshelar8251@gmail.com>
2023-10-24 12:10:44 +0530

    useEffect practice

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import UseState from './components/UseState';
 import './components/Hooks.css'
 import UseStateObject from './components/UseStateObject';
+import UseEffect from './components/UseEffect';
 
 function App() {
   return (
     <div className="App">
-      <UseState />
+      {/* <UseState /> */}
       {/* <UseStateObject /> */}
+      <UseEffect />
     </div>
   );
 }
--- src/components/UseEffect.jsx
+++ src/components/UseEffect.jsx
@@ -1,11 +1,31 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 
 const UseEffect = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+
+    const [count, setCount] = useState(0);
+
+    const countUpdate = (val) => {
+        if (val === "inc") return setCount(count + 1);
+        if (val === "dec") return setCount(count - 1);
+    }
+
+    useEffect(() => {
+        document.title = count;
+    });
+
+    return (
+        <div>
+            <div className='useState'>
+                <div className='innerClass'>
+                    <button className='incBtn' onClick={() => countUpdate("inc")}>+</button>
+                    <h2 className='count'>{count}</h2>
+                    <button className='decBtn' onClick={() => (count === 0) ? setCount(0) : setCount(count - 1)}>-</button>
+                </div>
+                <button className='decBtn' onClick={() => countUpdate("dec")}>Reset</button>
+            </div>
+        </div>
+    )
 }
 
 export default UseEffect
