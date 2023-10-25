ef34e97d79878f1cad1d5562764df87392014532
Harshal-creator <harshalshelar8251@gmail.com>
2023-10-25 12:37:27 +0530

    props example added

--- src/components/props/Props1.jsx
+++ src/components/props/Props1.jsx
@@ -3,16 +3,16 @@ import Props2 from './Props2'
 
 const Props1 = () => {
     const jsonData = {
-        name : 'harshal',
-        age : 25,
-        email : 'harshalshelar8251@gmail.com',
-        salary : '65000'
+        name: 'harshal',
+        age: 25,
+        email: 'harshalshelar8251@gmail.com',
+        salary: '65000'
     }
-  return (
-    <div>
-      <Props2 jsonData = {jsonData} name="Ihechikara" tool="Figma"/>
-    </div>
-  )
+    return (
+        <div>
+            <Props2 jsonData={jsonData} name="Ihechikara" tool="Figma" />
+        </div>
+    )
 }
 
 export default Props1;
